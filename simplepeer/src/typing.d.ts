a5c7c5bf6cb8b6021d2caff5c32005ccd6a89b84
Amber LIU <AmberLiu@guest-wireless-upc-1603-10-120-048-108.usc.edu>
2017-10-07 19:37:54 -0700

    add video chat app

--- simplepeer/src/typing.d.ts
+++
@@ -1,1 +0,0 @@
-declare var SimplePeer: any;
--- videoChat/e2e/app.e2e-spec.ts
+++ videoChat/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('simplepeer App', () => {
+describe('video-chat App', () => {
   let page: AppPage;
 
   beforeEach(() => {
